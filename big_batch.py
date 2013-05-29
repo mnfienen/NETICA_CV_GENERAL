b953e02c977af78d71228c8c202d87a2b059c170
Michael Fienen <mnfienen@usgs.gov>
2013-05-29 16:09:25 -0500

    Fixed bug in bigbatch.py that was preventing all codes from executing

--- big_batch.py
+++ big_batch.py
@@ -1,10 +1,10 @@
 import os
 
-numfolds = 6
+numfolds = 8
 baseroot = 'SEEDA1A1B7D7E7'
-basecases = ['log','sqrt','sqrt_dist','noxform']
+basecases = ['sqrt_dist']
 allfiles = os.listdir(os.getcwd())
-'''
+
 for ccas in basecases:
     runline1 = 'python Divide_CAS.py %s%s.cas %d' %(baseroot,ccas,numfolds)
     print runline1
@@ -13,12 +13,15 @@ for ccas in basecases:
     print runline2
     os.system(runline2)
 
+
+allfiles = os.listdir(os.getcwd())
 for cfile in allfiles:
     if cfile[-4:] == '.xml':
         runstring = 'python CV_driver.py %s' %(cfile)
         print runstring
         os.system(runstring)
-''' 
+
+allfiles = os.listdir(os.getcwd())
 for cfile in allfiles:
     if cfile[-4:] == '.dat' and ('CAL' in cfile or 'VAL' in cfile):
         runstring = 'python postproc_summarize_kfold.py %s' %(cfile)
